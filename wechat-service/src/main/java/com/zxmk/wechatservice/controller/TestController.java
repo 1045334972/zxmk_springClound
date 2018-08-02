ea46bfa57a8b1689f702054aff8b426d3ec8916c
1045334972 <zsp0808@qq.com>
2018-08-02 11:15:37 +0800

    IDEA整合GitHub测试

--- wechat-service/src/main/java/com/zxmk/wechatservice/controller/TestController.java
+++ wechat-service/src/main/java/com/zxmk/wechatservice/controller/TestController.java
@@ -3,7 +3,7 @@ package com.zxmk.wechatservice.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("test");
-        System.out.println("");
+        System.out.println("aaa");
     }
 }
 
